72126244fa2dcf6ead04a048eff88b7186b1581c
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-11-01 23:44:14 -0700

    added moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+// add current date to the page
+today = $("#today")
+  .text(moment().format("[cityName] (MM/DD/YYYY)"));
+
+tPlusOne = $("#t-plus-1")
+  .text(moment().add(1, "days").format("MM/DD/YYYY"));
+
+tPlusTwo = $("#t-plus-2")
+  .text(moment().add(2, "days").format("MM/DD/YYYY"));
+
+tPlusThree = $("#t-plus-3")
+  .text(moment().add(3, "days").format("MM/DD/YYYY"));
+
+tPlusFour = $("#t-plus-4")
+  .text(moment().add(4, "days").format("MM/DD/YYYY"));
+
+tPlusFive = $("#t-plus-5")
+  .text(moment().add(5, "days").format("MM/DD/YYYY"));
+
+
+
 fetch("https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=1e88f1bc9b5f8c56e02828ddcd511ef1")
